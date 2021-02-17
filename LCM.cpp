392cf542e14b3906d54c45c8dd875dac4aa3b29c
Aman2001 <gaman2001@gmail.com>
2021-02-17 13:52:34 +0530

    added inprogress program to find lcm (needs work)

---
+++ LCM.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int prime(int num1)
+{
+    while (num1%2==0)
+    {
+         
+    }
+}
+int main()
+{   int num1,num2;
+    cout<<"Enter the 2 numbers\n";
+    cin>> num1>>num2;
+   prime(num1);
+    return 0;
+}
